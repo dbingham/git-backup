e53343a93d3da36f3ba31782b87451135b352f86
Daniel Bingham <git@dbingham.com>
2017-06-13 13:52:02 -0700

    Tweak install script

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #
 
-cp --remove-destination --force git-backup  /usr/lib/git-core/
-cp --remove-destination --force git-restore /usr/lib/git-core/
+#cp --remove-destination --force git-backup  /usr/lib/git-core/
+if [ -e ~/Scripts/git-backup ]; then
+	rm ~/Scripts/git-backup
+fi
+ln -s ./git-backup ~/Scripts/
+#cp --remove-destination --force git-restore /usr/lib/git-core/
+if [ -e ~/Scripts/git-restore ]; then
+	rm ~/Scripts/git-restore
+fi
+ln -s ./git-restore ~/Scripts/
 
-gzip --to-stdout manpages/git-backup.1  > /usr/share/man/man1/git-backup.1.gz
-gzip --to-stdout manpages/git-restore.1 > /usr/share/man/man1/git-restore.1.gz
+gzip --to-stdout manpages/git-backup.1  > /usr/local/share/man/man1/git-backup.1.gz
+gzip --to-stdout manpages/git-restore.1 > /usr/local/share/man/man1/git-restore.1.gz
